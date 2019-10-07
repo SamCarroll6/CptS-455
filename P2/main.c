fab774f577fc6f48d90fe7c039de7112afe7f68b
Sam Carroll <cfcarroll321@gmail.com>
2019-10-06 17:58:04 -0700

    Making some progress, haven't tested yet, just want to save work.

--- P2/main.c
+++ P2/main.c
@@ -14,10 +14,82 @@
 #include <sys/socket.h>
 #include <net/if.h>
 #include <netinet/ether.h>
+#include <netinet/in.h>
+#include <assert.h>
 
 #define BUF_SIZ		65536
 #define SEND 0
 #define RECV 1
+#define ARP  2
+
+struct arp_hdr {
+    uint16_t       ar_hrd;
+    uint16_t       ar_pro;
+    unsigned char  ar_hln;
+    unsigned char  ar_pln;
+    uint16_t       ar_op;
+    unsigned char  ar_sha[6]; 
+    unsigned char  ar_sip[4];
+    unsigned char  ar_tha[6];
+    unsigned char  ar_tip[4];
+};
+
+void ARP_SendReply(char interfaceName[], char IP_Add[])
+{
+    struct in_addr addr;
+    struct arp_hdr hdr;
+    struct ifreq if_idx, if_ifr;
+    struct sockaddr_ll sk_addr = {0};
+
+    const unsigned char broadcast_addr = {0xff,0xff,0xff,0xff,0xff,0xff};
+
+    int sockfd = 0, i = 0;
+    inet_aton(IP_Add, &addr);
+    if((sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP))) < 0)
+    {
+        perror("socket failed\n");
+        exit(1);
+    }
+
+    memset(&if_ifr, 0, sizeof(struct ifreq));
+    strncpy(if_ifr.ifr_name, interfaceName, IFNAMSIZ - 1);
+    if(ioctl(sockfd, SIOCGIFINDEX, &if_ifr) < 0)
+    {
+        perror("SIOCGIFINDEX");
+    }
+
+    sk_addr.sll_ifindex = if_ifr.ifr_ifindex;
+    sk_addr.sll_family = AF_PACKET;
+    sk_addr.sll_halen = ETH_ALEN;
+    sk_addr.sll_protocol = ETH_P_ARP;
+    memcpy(sk_addr.sll_addr, broadcast_addr, ETHER_ADDR_LEN);
+
+    hdr.ar_hrd = htons(ARPHRD_ETHER);
+    hdr.ar_pro = htons(ETH_P_IP);
+    hdr.ar_hln = ETH_ALEN;
+    hdr.ar_pln = sizeof(in_addr_t);
+    hdr.ar_op = htons(ARPOP_REQUEST);
+    memset(&hdr.ar_tha, 0, sizeof(hdr.ar_tha));
+    memcpy(&hdr.ar_tip, &addr.s_addr, sizeof(hdr.ar_tip));
+
+    printf("IPADD = %s\n", inet_ntoa(addr));
+
+    memset(&if_idx, 0, sizeof(struct ifreq));
+    strncpy(if_idx.ifr_name, interfaceName, IFNAMSIZ - 1);
+    if(ioctl(sockfd, SIOCGIFADDR, &if_idx) < 0)
+    {
+        perror("SIOCGIFADDR");
+    }
+
+    
+
+    for(i = 0; i < ETH_ALEN; i++)
+    {
+        printf("%d:",((uint8_t*)&if_idx.ifr_hwaddr.sa_data)[i]);
+    }
+
+    printf("%d\n", ((struct sockaddr_in *)&if_idx.ifr_addr)->sin_addr.s_addr);
+}
 
 void send_message(char hw_addr[], char interfaceName[], char buf[]){
 
@@ -144,6 +216,7 @@ int main(int argc, char *argv[])
 	char hw_addr[6];
 	char interfaceName[IFNAMSIZ];
 	char buf[BUF_SIZ];
+    char IP_Add[20];
 	memset(buf, 0, BUF_SIZ);
 	
 	int correct=0;
@@ -156,18 +229,29 @@ int main(int argc, char *argv[])
 				correct=1;
 				printf("  buf: %s\n", buf);
 			}
+            strncpy(interfaceName, argv[2], IFNAMSIZ);
 		}
 		else if(strncmp(argv[1],"Recv", 4)==0){
 			if (argc == 3){
 				mode=RECV;
 				correct=1;
 			}
+            strncpy(interfaceName, argv[2], IFNAMSIZ);
 		}
-		strncpy(interfaceName, argv[2], IFNAMSIZ);
+        else
+        {
+            assert(argc > 2);
+            strncpy(interfaceName, argv[1], IFNAMSIZ);
+            //inet_aton(argv[2], &IPhold);
+            strcpy(IP_Add, argv[2]);
+            mode = ARP;
+            correct = 1;
+        }
 	 }
 	 if(!correct){
 		fprintf(stderr, "./455_proj2 Send <InterfaceName>  <DestHWAddr> <Message>\n");
 		fprintf(stderr, "./455_proj2 Recv <InterfaceName>\n");
+        fprintf(stderr, "./455_proj2 <InterfaceName> <IP Address>");
 		exit(1);
 	 }
 	//Do something here
@@ -178,6 +262,10 @@ int main(int argc, char *argv[])
 	else if (mode == RECV){
 		recv_message(interfaceName);
 	}
+    else if(mode == ARP)
+    {
+        ARP_SendReply(interfaceName, IP_Add);
+    }
 
 	return 0;
 }
